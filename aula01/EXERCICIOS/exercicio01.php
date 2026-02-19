41a06368f40a83c2f2c739ad2872c05ca4fa3fd8
katarina2008 <160730590+katarina2008@users.noreply.github.com>
2026-02-19 12:20:41 -0300

    Add files via upload

---
+++ aula01/EXERCICIOS/exercicio01.php
@@ -0,0 +1,12 @@
+<?php
+$nome = "Katarina";
+
+echo "Bem vinda $nome"
+
+
+
+
+
+
+
+?>
---
+++ aula01/EXERCICIOS/exercicio02.php
@@ -0,0 +1,17 @@
+<?php
+$nota1 = 10;
+$nota2 = 7;
+$nota3 = 9;
+$media = (($nota1+$nota2+$nota3)/3);
+
+if  ($media >= 7){
+
+    echo "Média: " . $media . "- APROVADO!";
+
+} else {
+    echo "Média: " . $media . "- REPROVADO!";
+}
+
+
+
+?>
