882f580ac1bc006e5d5ad0ee5ff562d118664c20
katarina2008 <160730590+katarina2008@users.noreply.github.com>
2026-03-02 20:02:26 -0300

    Add files via upload

---
+++ aula02/exercicio03.php
@@ -0,0 +1,12 @@
+<?php
+
+$i = 10;
+
+if ($i % 2  == 0) {
+    echo $i . " é um número par" . "<br>";
+} else {
+    echo $i . " é um número ímpar" . "<br>";
+
+}
+
+?>
---
+++ aula02/exercicio08.php
@@ -0,0 +1,10 @@
+<?php
+$n = 8;
+
+echo "Tabuada do ". $n ."<br>";
+
+for($i = 1; $i <= 10; $i++){
+    $multiplicacao = $n * $i;
+    echo $n ." x ". $i ." = ". $multiplicacao ."<br>";
+}
+?>
